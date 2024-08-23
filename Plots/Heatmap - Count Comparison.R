75fa166b553d2c6d829a5543d14c0f594093de70
Lucas Boldrini <68539180+LucasBoldrini@users.noreply.github.com>
2024-08-23 17:58:23 +0200

    Add files via upload

---
+++ Plots/Heatmap - Count Comparison.R
@@ -0,0 +1,39 @@
+# Load required libraries
+library(ggplot2)
+library(reshape2)
+library(colorspace)
+library(readxl)
+
+# Read the data from an Excel file
+data <- read_excel("Heatmap_keywords.xlsx")
+
+# Reshape the data into a long format for ggplot2
+data_melt <- melt(data, id.vars = "Keywords", variable.name = "URL_Type", value.name = "Hits")
+
+# Create a custom color palette using colorspace
+#custom_palette <- sequential_hcl(100, palette = "Rocket")
+custom_palette <- rev(sequential_hcl(100, palette = "Rocket"))  # Invert the color palette
+
+# Create the heatmap with the custom color palette and no borders
+heatmap <- ggplot(data_melt, aes(x = Keywords, y = URL_Type, fill = Hits)) +
+  geom_tile(color = "white") +
+  scale_fill_gradientn(colors = custom_palette) +  # Apply the custom color palette
+  labs(title = "Heatmap of Keyword Hits by URL Type", x = NULL, y = NULL) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 9),
+    axis.text.y = element_text(angle = 0, hjust = 1, size = 12),
+    plot.title = element_text(hjust = 0.5),
+    panel.background = element_rect(fill = "white"),
+    plot.background = element_rect(fill = "white"),
+    panel.grid = element_blank(),  # Remove gridlines
+    axis.ticks = element_blank(),  # Optionally remove axis ticks
+    panel.border = element_blank(),  # Remove panel border
+    plot.margin = margin(30, 30, 5, 5)  # Remove margins around the plot
+  ) +
+  coord_fixed(ratio = 1)  # Ensure tiles are squares
+
+heatmap
+
+# Save the plot with specific dimensions
+# ggsave("Heatmap_keywords_no_border.png", plot = heatmap, width = 10, height = 6, dpi = 300, units = "in", bg = "white")
